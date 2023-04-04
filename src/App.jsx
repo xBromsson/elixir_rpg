4b6afea8c3f6c803013020ef0b9ec4c096fcdbfd
Marvin Brown <marvin@the-firebrand.com>
2023-04-04 13:25:11 -0500

    built basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { Text } from "@chakra-ui/react";
-import ColorModeSwitch from "./components/ColorModeSwitch";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <Text>Hello World</Text>
-      <ColorModeSwitch></ColorModeSwitch>
-    </div>
+    <Grid
+      h="100vh"
+      templateAreas={`"nav header" 
+                      "nav main"`}
+      templateColumns={"225px 1fr"}
+      templateRows={"75px 1fr"}
+    >
+      <GridItem bg="gray.600" area={"nav"}></GridItem>
+      <GridItem bg="gray.700" area={"header"}></GridItem>
+      <GridItem bg="gray.800" area={"main"}></GridItem>
+    </Grid>
   );
 }
 
